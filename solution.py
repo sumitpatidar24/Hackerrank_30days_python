74ceea159188fed14e0fcc29ae7ef1b53159cad7
sumitpatidar24 <47334403+sumitpatidar24@users.noreply.github.com>
2020-10-19 17:15:18 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,18 +4,18 @@ for i in range(6):
     arr.append(a)
 def func_of_sum(mat,row,col):
     sums=0
-    sums+=mat[row][col-1]
+    sums+=mat[row-1][col-1]
+    sums+=mat[row-1][col]
+    sums+=mat[row-1][col+1]
     sums+=mat[row][col]
-    sums+=mat[row][col+1]
+    sums+=mat[row+1][col-1]
     sums+=mat[row+1][col]
-    sums+=mat[row+2][col-1]
-    sums+=mat[row+2][col]
-    sums+=mat[row+2][col+1]
+    sums+=mat[row+1][col+1]
     return sums
 sum_list=[]
 
-for x in range(4):
-    for y in range(4):
+for x in range(1,5):
+    for y in range(1,5):
         summing=func_of_sum(arr,x,y)
         sum_list.append(summing)
             
