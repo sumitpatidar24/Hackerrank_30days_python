259223553fb9cbfd33070f4215d537cad523e35a
sumitpatidar24 <47334403+sumitpatidar24@users.noreply.github.com>
2020-10-21 18:41:16 +0530

    Create day_1.py

---
+++ day_1.py
@@ -0,0 +1,13 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+d=4.0
+s="HackerRank"
+inte=int(input())
+flo=float(input())
+stri=str(input())
+
+print(inte+i)
+print(flo+i)
+print(s+" "+stri)
